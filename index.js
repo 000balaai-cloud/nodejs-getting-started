f7f9f10388d0d4ab60d3beeb3c0c41c51c58d3a5
000balaai-cloud <000balaai@gmail.com>
2026-01-08 09:04:55 +0530

    Update index.js
    Signed-off-by: 000balaai-cloud <000balaai@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const axios = require("axios");
+console.log("TOKEN LENGTH:", process.env.WHATSAPP_TOKEN?.length);
 
 const app = express();
 app.use(express.json());
